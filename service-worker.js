a6794308989471810d831abee420eefe72f4f0ee
AkashChaudhary7 <ictlabgsssaidana@gmail.com>
2025-12-26 19:21:58 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lifeos-v1-cache';
+const CACHE_NAME = 'lifeos-adaptive-v1';
 const ASSETS = [
     './',
     './index.html',
@@ -7,10 +7,12 @@ const ASSETS = [
     './manifest.json'
 ];
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(response => response || fetch(e.request)));
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => res || fetch(e.request))
+    );
 });
